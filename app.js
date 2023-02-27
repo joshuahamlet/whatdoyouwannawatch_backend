d042ab146503dd1ab7726aff83db351770728459
Joshua Hamlet <Joshua7hamlet@gmail.com>
2023-02-27 15:23:37 -0800

    fix: updates for communicating with client

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.set('trust proxy', 1)
 
 require('express-async-errors')
 require('dotenv/config')
@@ -12,26 +13,30 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const GoogleUser = require('./models/googleUserModel')
 
-app.use(cors())
+app.use(
+  cors({
+    credentials: true,
+    origin: 'https://whatdoyouwannawatch-backend.web.app',
+  })
+)
 
 ///////
-const PORT = 4000
+const PORT = 8080
 const server = require('http').createServer(app)
 server.listen(PORT)
 console.log(`listening on ${PORT}`)
 
 const io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:3002",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: 'https://whatdoyouwannawatch-backend.web.app',
+    methods: ['GET', 'POST'],
+  },
 })
 
-io.on('connection', socket => {
-    
+io.on('connection', (socket) => {
   console.log(`a user connected: ${socket.id}`)
-  
-  socket.on('send-message', message => {
+
+  socket.on('send-message', (message) => {
     socket.broadcast.emit('receive-message', message.message + ' Howdy')
     socket.emit('receive-message', message.message + ' EMIT')
   })
@@ -39,102 +44,112 @@ io.on('connection', socket => {
 
 ////////
 
-
 //////////{ Cookie Setup}
-app.use(cookieSession({
+app.use(
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY]
-}))
+    keys: [process.env.COOKIE_KEY],
+    sameSite: 'none',
+    secure: true,
+  })
+)
 app.use(passport.initialize())
 app.use(passport.session())
 //^^^^^^^^//^^^^^^^^//
 
 //////////{ Parser }
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 //^^^^^^^^//^^^^^^^^//
-
 
 //////////{ Google OAuth }
 passport.serializeUser((user, done) => {
-    done(null, user.id)
+  done(null, user.id)
 })
 
 passport.deserializeUser((id, done) => {
-    GoogleUser.findById(id)
-        .then(user => {
-            done(null, user)
-        })
+  GoogleUser.findById(id).then((user) => {
+    done(null, user)
+  })
 })
 
 passport.use(
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        GoogleUser.findOne({ googleId: profile.id })
-            .then(existingUser => {
-                if (existingUser) {
-                    done(null, existingUser)
-                } else {
-                    new GoogleUser({
-                            googleId: profile.id,
-                            username: profile.displayName,
-                            email: profile.emails[0].value,
-                            usericon: profile.photos[0].value
-                        })
-                        .save()
-                        .then(user => done(null, user))
-                }
-            })
-    }))
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'),
-    (req, res) => {
-        res.redirect('/')
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        'https://whatdoyouwannawatchbackend.fly.dev/auth/google/callback',
+      passReqToCallback: true,
+      scope: ["profile", "email"]
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      console.log('PROFILE', profile)
+      GoogleUser.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser)
+        } else {
+          new GoogleUser({
+            googleId: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            usericon: profile.photos[0].value,
+          })
+            .save()
+            .then((user) => done(null, user))
+        }
+      })
     }
+  )
+)
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    includeGrantedScopes: true,
+    prompt: 'select_account',
+  })
+)
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {successRedirect: "https://whatdoyouwannawatch-backend.web.app"}),
+  (req, res) => {
+    console.log("User ", req.user )
+    res.send("Thank you for signing in!")
+  }
 )
 
 app.get('/api/logout', (req, res) => {
-    req.logout()
-    res.send(req.user)
+  req.logout()
+  res.send(req.user)
 })
 
 app.get('/api/current_user', async (req, res) => {
-    //res.json(req.user)
-    try {
-        const currentUser = await GoogleUser
-        .findOne({ _id: req.user._id })
-        .populate('movies')
-        .populate('friends', ['movies', 'usericon', 'username'])
+  //res.json(req.user)
+  try {
+    const currentUser = await GoogleUser.findOne({ _id: req.user._id })
+      .populate('movies')
+      .populate('friends', ['movies', 'usericon', 'username'])
+console.log("THIS IS THE CURRUSR", currentUser)
+    const { friends } = currentUser
 
-        const { friends } = currentUser
-          
-        const promises = friends.map(async friend => {
-          const friendPop = await friend.populate('movies').execPopulate()
-          return friendPop
-        })
-          
-        const userFriends = await Promise.all(promises)
+    const promises = friends.map(async (friend) => {
+      const friendPop = await friend.populate('movies').execPopulate()
+      return friendPop
+    })
 
-        return res.json({currentUser, userFriends})
-
-    } catch (err) {
-        return res.json({message: err})         
-    }
+    const userFriends = await Promise.all(promises)
+    return res.json({ currentUser, userFriends })
+  } catch (err) {
+    return res.json({ message: err })
+  }
 })
 //^^^^^^^^//^^^^^^^^//
 
-
 //////////{ Routes }
-app.get('/', (req,res) => {
-    res.send('We are on home')
+app.get('/', (req, res) => {
+  res.send('We are on home')
 })
 
 const userRoutes = require('./routes/userRoutes')
@@ -146,9 +161,13 @@ const { Console } = require('console')
 app.use('/movies', movieRoutes)
 //^^^^^^^^//^^^^^^^^//
 
-
 mongoose.connect(
-    process.env.DB_CONNECTION, 
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, 
-    () => console.log('connected to DB!') 
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  () => console.log('connected to DB!')
 )
